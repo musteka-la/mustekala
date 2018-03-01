82097679138eefdeaa496be98d88e071c169d8ae
Herman Junge <herman.junge@consensys.net>
2018-02-28 23:30:04 -0300

    scrapper value adjustements

--- devp2p/peer-scrapper.go
+++ devp2p/peer-scrapper.go
@@ -1,6 +1,10 @@
 package devp2p
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+	"time"
+)
 
 type regexMessages struct {
 	statusMsg        *regexp.Regexp
@@ -34,43 +38,46 @@ func (m *Manager) peerScrapper(peerid, status, statusPlus string) {
 		return
 	}
 
-	var toDBKey, toDBValue string
+	var toDBKey, toDBVal string
 
 	// cleanup
 	if peerid[:4] == "Peer" {
 		peerid = peerid[5:]
 	}
 
-	// make additional processing to the status, if needed
+	// make additional processing to the status if needed
+	// just csv friendly commas
 	switch {
-	case status == "failed-eth-handshake":
+	case status == "39-ethereum handshake failed":
 		switch {
 		case peerScrapperRegex.statusMsg.MatchString(statusPlus):
-			statusPlus = "status message\",\"" + statusPlus[16:]
+			statusPlus = "status message, " + statusPlus[16:]
 		case peerScrapperRegex.msgTooLarge.MatchString(statusPlus):
-			statusPlus = "message too large\",\"" + statusPlus[19:]
+			statusPlus = "message too large, " + statusPlus[19:]
 		case peerScrapperRegex.decoding.MatchString(statusPlus):
-			statusPlus = "rlp decoding\",\"" + statusPlus[16:]
+			statusPlus = "rlp decoding, " + statusPlus[16:]
 		case peerScrapperRegex.networkMismatch.MatchString(statusPlus):
-			statusPlus = "network mismatch\",\"" + statusPlus[18:]
+			statusPlus = "network mismatch, " + statusPlus[18:]
 		case peerScrapperRegex.genesisMismatch.MatchString(statusPlus):
-			statusPlus = "genesis block mismatch\",\"" + statusPlus[24:]
+			statusPlus = "genesis block mismatch, " + statusPlus[24:]
 		case peerScrapperRegex.protocolMismatch.MatchString(statusPlus):
-			statusPlus = "p2p protocol mismatch\",\"" + statusPlus[27:]
+			statusPlus = "p2p protocol mismatch, " + statusPlus[27:]
 		default:
 			statusPlus = statusPlus
 		}
 	}
 
-	// TODO
 	// fill the key and value to the DB accordingly
+	toDBKey = "peer " + peerid
+	toDBVal = fmt.Sprintf("%d, %s, %s", time.Now().UnixNano(), status, statusPlus)
+
+	// DEBUG
+	fmt.Printf("peerScrapper:\nkey:\t%v\nval:\t%v\n", toDBKey, toDBVal)
+	// DEBUG
 
 	// TODO
-	// (just in memory case)
-	// Just fmt.Printf your key and value!
-
-	// TODO
-	// sending to the db
-	_ = toDBKey
-	_ = toDBValue
+	// 0. Send the peer to the set devp2p-peers (will create if it does not exist)
+	// 1. Send the status to the set devp2p-peerstatus:<peerid>
+	// 2. If it is a byzantium peer, send it to the set devp2p-byzantium-peers
+	// 3. If we have a negative for byzantium (wrong genesis, network or fork), send it to the set: devp2p-not-byzantium
 }
