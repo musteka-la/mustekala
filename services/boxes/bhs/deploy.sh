ee2e31fd9eb952ed1fbdd8e30c1d6826388b0839
Herman Junge <chpdg42@gmail.com>
2018-04-11 11:30:21 -0300

    Deploy BHS

--- services/boxes/bhs/deploy.sh
+++ services/boxes/bhs/deploy.sh
@@ -36,7 +36,6 @@ ssh $REMOTE_MACHINE mkdir -p $REMOTE_HOME_DIR
 scp $CURRENT_DIR/prepare_box $REMOTE_MACHINE:$REMOTE_HOME_DIR/prepare_box
 scp $CURRENT_DIR/run_box $REMOTE_MACHINE:$REMOTE_HOME_DIR/run_box
 scp $CURRENT_DIR/stop_box $REMOTE_MACHINE:$REMOTE_HOME_DIR/stop_box
-scp $CURRENT_DIR/Dockerfile $REMOTE_MACHINE:$REMOTE_HOME_DIR/Dockerfile
 scp $CURRENT_DIR/docker-compose.yml $REMOTE_MACHINE:$REMOTE_HOME_DIR/docker-compose.yml
 
 # Actual commands: Execution
@@ -45,4 +44,12 @@ echo -e "\nExecuting preparation of box\n"
 
 ssh $REMOTE_MACHINE $REMOTE_HOME_DIR/prepare_box
 
-# TODO: stop_box and run_box
+# Finally stop_box and run_box
+
+echo -e "\nEverything set. Go for it\n"
+
+ssh $REMOTE_MACHINE "cd $REMOTE_HOME_DIR && $REMOTE_HOME_DIR/stop_box"
+ssh $REMOTE_MACHINE "cd $REMOTE_HOME_DIR && $REMOTE_HOME_DIR/run_box"
+
+# A check and we are good to go
+ssh $REMOTE_MACHINE docker ps -a
